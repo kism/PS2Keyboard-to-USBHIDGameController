baa9e8be3cfc8f9c1c26158bf980221a32595c75
Kieran Gee <kieran.lost.the.game@gmail.com>
2020-12-06 13:09:54 +1100

    Take that yanderedev

--- PS2ARDUINO2803BROOK/PS2ARDUINO2803BROOK.ino
+++ PS2ARDUINO2803BROOK/PS2ARDUINO2803BROOK.ino
@@ -1,4 +1,4 @@
-/* 
+/*
 PS/2 To Brook Fighting Board
 For the Teensy, only tested on the Teensy-LC
 Make sure you get the latest git version of PS2keyboard
@@ -6,28 +6,28 @@ Make sure you get the latest git version of PS2keyboard
 
 
 #include <PS2Keyboard.h>
-#include <digitalWriteFast.h>
+//#include <digitalWriteFast.h>
 
 const int DataPin = 4; //The data pin is Pin 1 of the MiniDin-6 (PS/2) connector
-const int IRQpin =  3; //The clock pin is Pin 5 of the MiniDin-6 (PS/2) connector
+const int IRQpin = 3; //The clock pin is Pin 5 of the MiniDin-6 (PS/2) connector
 
 PS2Keyboard keyboard;
 
-const int dpadup = 6;   
-const int dpaddown = 7; 
-const int dpadleft = 8; 
+const int dpadup = 6;
+const int dpaddown = 7;
+const int dpadleft = 8;
 const int dpadright = 9;
 
 const int sel = 10;
 const int star = 11;
 const int psbutton = 12;
 
-const int squar = 14;  
+const int squar = 14;
 const int triangle = 15;
 const int r1 = 16;
 const int l1 = 17;
 
-const int cross = 18;  
+const int cross = 18;
 const int circle = 19;
 const int r2 = 20;
 const int l2 = 21;
@@ -57,83 +57,131 @@ void setup() {
 
 void loop() {
   int scn = getNextPS2Event();
+
   //Main key release events
-  if (scn == 0xF0){
+  if (scn == 0xF0) {
     scn = getNextPS2Event();
-    if (scn == 0x1C) {   //A
+
+    switch (scn) {
+    case 0x1C:
       digitalWriteFast(squar, LOW);
-    } else if (scn == 0x1B) { //S
+      break;
+    case 0x1B:
       digitalWriteFast(triangle, LOW);
-    } else if (scn == 0x23) { //D
+      break;
+    case 0x23:
       digitalWriteFast(r1, LOW);
-    } else if (scn == 0x2B) { //F
+      break;
+    case 0x2B:
       digitalWriteFast(l1, LOW);
-    } else if (scn == 0x1A) { //Z
+      break;
+    case 0x1A:
       digitalWriteFast(cross, LOW);
-    } else if (scn == 0x22) { //X
+      break;
+    case 0x22:
       digitalWriteFast(circle, LOW);
-    } else if (scn == 0x21) { //C
+      break;
+    case 0x21:
       digitalWriteFast(r2, LOW);
-    } else if (scn == 0x2A) { //V
+      break;
+    case 0x2A:
       digitalWriteFast(l2, LOW);
-    } else if (scn == 0x16) { //1
+      break;
+    case 0x16:
       digitalWriteFast(star, LOW);
-    } else if (scn == 0x2E) { //5
+      break;
+    case 0x2E:
       digitalWriteFast(sel, LOW);
-    } else if (scn == 0x46) { //5
+      break;
+    case 0x46:
       digitalWriteFast(psbutton, LOW);
+      break;
+    default:
+      break;
     }
   }
+
   //Arrow events
   else if (scn == 0xE0) {
-    scn = getNextPS2Event();    
+    scn = getNextPS2Event();
+
     //Arrow key release events
     if (scn == 0xF0) {
       scn = getNextPS2Event();
-      if (scn == 0x75) {   //Up
+      switch (scn) {
+      case 0x75:
         digitalWriteFast(dpadup, LOW);
-      } else if (scn == 0x72) { //Down
+        break;
+      case 0x72:
         digitalWriteFast(dpaddown, LOW);
-      } else if (scn == 0x6B) { //Left
+        break;
+      case 0x6B:
         digitalWriteFast(dpadleft, LOW);
-      } else if (scn == 0x74) { //Right
+        break;
+      case 0x74:
         digitalWriteFast(dpadright, LOW);
+        break;
+      default:
+        break;
       }
     }
-    //Arrow Key Events
-    else if (scn == 0x75) {   //Up
-      digitalWriteFast(dpadup, HIGH);
-    } else if (scn == 0x72) { //Down
-      digitalWriteFast(dpaddown, HIGH);
-    } else if (scn == 0x6B) { //Left
-      digitalWriteFast(dpadleft, HIGH);
-    } else if (scn == 0x74) { //Right
-      digitalWriteFast(dpadright, HIGH);
+    else {
+      switch (scn) {
+      case 0x75:
+        digitalWriteFast(dpadup, HIGH);
+        break;
+      case 0x72:
+        digitalWriteFast(dpaddown, HIGH);
+        break;
+      case 0x6B:
+        digitalWriteFast(dpadleft, HIGH);
+        break;
+      case 0x74:
+        digitalWriteFast(dpadright, HIGH);
+        break;
+      default:
+        break;
+      }
     }
   }
-  //Regular key press events
-  else if (scn == 0x1C) {   //A
+
+  //Regular Key press events
+  switch (scn) {
+  case 0x1C:
     digitalWriteFast(squar, HIGH);
-  } else if (scn == 0x1B) { //S
+    break;
+  case 0x1B:
     digitalWriteFast(triangle, HIGH);
-  } else if (scn == 0x23) { //D
+    break;
+  case 0x23:
     digitalWriteFast(r1, HIGH);
-  } else if (scn == 0x2B) { //F
+    break;
+  case 0x2B:
     digitalWriteFast(l1, HIGH);
-  } else if (scn == 0x1A) { //Z
+    break;
+  case 0x1A:
     digitalWriteFast(cross, HIGH);
-  } else if (scn == 0x22) { //X
+    break;
+  case 0x22:
     digitalWriteFast(circle, HIGH);
-  } else if (scn == 0x21) { //C
+    break;
+  case 0x21:
     digitalWriteFast(r2, HIGH);
-  } else if (scn == 0x2A) { //V
+    break;
+  case 0x2A:
     digitalWriteFast(l2, HIGH);
-  } else if (scn == 0x16) { //1
+    break;
+  case 0x16:
     digitalWriteFast(star, HIGH);
-  } else if (scn == 0x2E) { //5
+    break;
+  case 0x2E:
     digitalWriteFast(sel, HIGH);
-  } else if (scn == 0x46) { //5
+    break;
+  case 0x46:
     digitalWriteFast(psbutton, HIGH);
+    break;
+  default:
+    break;
   }
 }
 
